f2ec78d848fecc0ff2a0314aea4394a5252cda42
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-24 14:38:46 -0300

    agregando img

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const people = [
     name: 'Tanya Sinclair',
     job: 'UX Engineer',
     image:
-      'https://github.com/hgoo150925/coding-bootcamp-testimonials-slider/blob/main/src/assets/images/image-tanya.jpg',
+      'https://maikocode.github.io/Coding-bootcamp-testimonials-slider/images/image-tanya.jpg',
     text: '“I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.”',
   },
   {
@@ -12,7 +12,7 @@ export const people = [
     name: 'John Tarkpor',
     job: 'Junior Front-end Developer',
     image:
-      'https://github.com/hgoo150925/coding-bootcamp-testimonials-slider/blob/main/src/assets/images/image-john.jpg',
+      'https://maikocode.github.io/Coding-bootcamp-testimonials-slider/images/image-john.jpg',
     text: '“If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.”',
   },
 ];
