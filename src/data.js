332f720a549eea6cc3697b147933fc988a598d95
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-24 14:36:21 -0300

    agregando img

--- src/data.js
+++ src/data.js
@@ -3,14 +3,16 @@ export const people = [
     id: 1,
     name: 'Tanya Sinclair',
     job: 'UX Engineer',
-    image: '../src/assets/images/image-tanya.jpg',
+    image:
+      'https://github.com/hgoo150925/coding-bootcamp-testimonials-slider/blob/main/src/assets/images/image-tanya.jpg',
     text: '“I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.”',
   },
   {
     id: 2,
     name: 'John Tarkpor',
     job: 'Junior Front-end Developer',
-    image: '../src/assets/images/image-john.jpg',
+    image:
+      'https://github.com/hgoo150925/coding-bootcamp-testimonials-slider/blob/main/src/assets/images/image-john.jpg',
     text: '“If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.”',
   },
 ];
